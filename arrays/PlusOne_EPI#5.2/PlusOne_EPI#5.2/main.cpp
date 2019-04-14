b59b23233bf0a1cf47090af114f9cce89e00d946
dineshravilla <dinesh.ravilla7@gmail.com>
2019-04-14 13:31:49 -0400

    EPI#5.2_Leetcode#66

--- arrays/PlusOne_EPI#5.2/PlusOne_EPI#5.2/main.cpp
+++ arrays/PlusOne_EPI#5.2/PlusOne_EPI#5.2/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include "array.h"
+
+arrayOperations number;
+arrayOperations result;
+
+void plusOne(int *temp) {
+    
+    for(int i = number.size() - 1; i >= 0; i--) {
+        if(temp[i] < 9) {
+            temp[i]++;
+            number.display();
+            return;
+        }
+        temp[i] = 0;
+    }
+    
+    result.create(number.size()+1, 0);
+    result.arr[0] = 1;
+    result.display();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    number.create({1,9,9,3,9,9});
+    
+    //int *result;
+    
+    plusOne(number.arr);
+    
+    //result.display();
+    
     return 0;
 }
