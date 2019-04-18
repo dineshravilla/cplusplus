27840c3b551eb62a2198a4fc89b4169c97d4eb46
dineshravilla <dinesh.ravilla7@gmail.com>
2019-04-18 01:54:55 -0400

    spiral

--- TwoDimensionalArrays/Matrices/Matrices/main.cpp
+++ TwoDimensionalArrays/Matrices/Matrices/main.cpp
@@ -24,6 +24,7 @@ void swap(int &a, int &b) {
     b = temp;
 }
 
+//read matrix
 void readMatrix(matrix &m) {
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLS; j++) {
@@ -35,6 +36,7 @@ void readMatrix(matrix &m) {
     }
 }
 
+//display matrix
 void printMatrix(const matrix &m) {
     for(int i = 0; i < ROWS; i++) {
         for(int j = 0; j < COLS; j++) {
@@ -44,6 +46,7 @@ void printMatrix(const matrix &m) {
     }
 }
 
+//matrix addition
 void addMatrix(matrix &m1, matrix &m2) {
     matrix result(ROWS, row(COLS));
     for(int i = 0; i < ROWS; i++) {
@@ -54,6 +57,7 @@ void addMatrix(matrix &m1, matrix &m2) {
     printMatrix(result);
 }
 
+//transpose matrix
 void transposeMatrix(matrix &m) {
     //just upper and lower trianlge in the matrix
     //should be a square matrix
@@ -65,6 +69,7 @@ void transposeMatrix(matrix &m) {
     }
 }
 
+//symmetric matrix
 bool isSymmetric(const matrix &m) {
     //just upper and lower trianlge in the matrix
     //should be a square matrix
@@ -76,6 +81,36 @@ bool isSymmetric(const matrix &m) {
         }
     }
     return true;
+}
+
+//spiral order of a matrix (helper) and caller function
+void spiralTraversalHelper(const matrix &m, const int top, const int bottom, const int left, const int right, vector<int> &result) {    
+    if(left > right || top > bottom)
+        return;
+    for(int i = left; i <= right; i++) {
+        result.push_back(m[top][i]);
+    }
+    for(int i = top+1; i <= bottom && top != bottom; i++) {
+        result.push_back(m[i][right]);
+    }
+    for(int i = right-1; i >= left && top != bottom; i--) {
+        result.push_back(m[bottom][i]);
+    }
+    for(int i = bottom-1; i >= top+1 && top != bottom && left != right; i--) {
+        result.push_back(m[i][left]);
+    }
+    spiralTraversalHelper(m, top+1, bottom-1, left+1, right-1, result);
+}
+
+void spiralTraversal(const matrix &m) {
+    vector<int> result;
+    if(m.size() == 0 || m[0].size() == 0)
+        return;
+    spiralTraversalHelper(m, 0, (int)m.size()-1, 0, (int)m[0].size()-1, result);
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
 }
 
 void multiplyMatrix(const matrix &m1, const matrix &m2) {
@@ -96,10 +131,11 @@ void multiplyMatrix(const matrix &m1, const matrix &m2) {
 int main(int argc, const char * argv[]) {
     
     matrix m1(ROWS, row(COLS));
-    matrix m2(ROWS, row(COLS));
+    //matrix m2(ROWS, row(COLS));
     readMatrix(m1);
-    cout << endl << "Second Matrix." << endl;
-    readMatrix(m2);
+    
+    //cout << endl << "Second Matrix." << endl;
+    //readMatrix(m2);
     
     //addMatrix(m1, m2);
     //transposeMatrix(m1);
@@ -114,7 +150,9 @@ int main(int argc, const char * argv[]) {
     cout << endl;
     */
     
-    multiplyMatrix(m1, m2);
+    //multiplyMatrix(m1, m2);
+    
+    spiralTraversal(m1);
     
     return 0;
 }
